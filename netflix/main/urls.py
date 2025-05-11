2fc928a5662306f8e4e8fe9b47ab0a76077df5c9
Tadiwanashe Makoni <tadiwa.makoni13@gmail.com>
2025-05-11 21:02:53 +0200

    del pics

--- netflix/main/urls.py
+++ netflix/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
+    path('signin',views.signin,name='signin'),
+    path('signup',views.signup,name='signup'),
     path('watch_movie/<int:movie_id>',views.watch_movie,name='watch_movie'),
 ]
